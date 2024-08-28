850afbbeaaa38c964f0940ffadd89dd29e693334
Celine San Martin Cordeiro <celinesmc24@gmail.com>
2024-08-28 17:47:46 +0200

    main backend file (middleware, exports,...)

---
+++ presents-backend/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const jwt = require("jsonwebtoken");
+const activeApiKeys = require("./activeApiKeys");
+const port = 4000
+
+const app = express();
+
+var cors = require('cors');
+app.use(cors());
+
+app.use(express.json());
+
+// MIDDLEWARE
+
+app.use(["/"], (req,res,next) => {
+    console.log("middleware execution");
+
+    let apiKey = req.query.apiKey
+	if ( apiKey == undefined ){
+		res.status(401).json({ error: "no apiKey" });
+		return 
+	}
+	let infoInApiKey = jwt.verify(apiKey, "secret");
+	if ( infoInApiKey == undefined || activeApiKeys.indexOf(apiKey) == -1){
+		res.status(401).json({ error: "invalid apiKey" });
+		return 	
+	}
+
+	req.infoInApiKey = infoInApiKey;
+    next();
+})
+
+// ROUTERS
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
