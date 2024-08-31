fb92f3fbd5c670b112df9eab472ac5520a964717
celinesmc <celinesmc24@gmail.com>
2024-08-31 15:59:28 +0200

    added css version app.js

--- react-presents-html-css/src/App.js
+++ react-presents-html-css/src/App.js
@@ -1,10 +1,81 @@
+import { Route, Routes, Link, useNavigate, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+import { backendURL } from './Globals';
+
 import './App.css';
 
-function App() {
+let App = () => {
+  let [login, setLogin] = useState(false);
+  let [notification, setNotification] = useState("");
+  let navigate = useNavigate();
+  let location = useLocation();
+
+  useEffect(() => {
+    checkLogin();
+  },[])
+
+  let checkLogin = async () => {
+    if(localStorage.getItem("apiKey") != "null"){
+      let response = await fetch(backendURL+"/users/checkLogin?apiKey="+localStorage.getItem("apiKey"))
+      if (response.status == 401 || response.status == 500){ 
+        navigate("/login");
+        return
+      } else {
+        setLogin(true);
+      }
+    } else { 
+      let href = location.pathname 
+      if(["/login","/register"].includes(href) == false){ 
+        navigate("/login")
+      }
+      setLogin(false);
+    }
+  }
+
+  let disconnect = async () => {
+    let response = await fetch (backendURL+"/users/disconnect?apiKey="+localStorage.getItem("apiKey"))
+
+    if (response.ok){
+      localStorage.removeItem("apiKey")
+      setLogin(false)
+      navigate("/login")
+    }
+  }
+
+  let createNotification = (msg) => {
+    setNotification(msg);
+    setTimeout(() => {
+      setNotification("");
+    }, 3000)
+  }
+
   return (
     <div className="App">
-      <h1>Index</h1>
-    </div>
+      <nav>
+        <li><Link to="/">Index</Link></li>
+        { !login && <li><Link to="/register">Register</Link></li>}
+        { !login && <li><Link to="/login">Login</Link></li>}
+        { login && <li><Link to="/createPresent">Create presents</Link></li>}
+        { login && <li><Link to="/myPresents">My presents</Link></li>}
+        { login && <li><Link to="/friends">Friends list</Link></li>}
+        { login && <li><Link to="#" onClick={disconnect}>Disconnect</Link></li>}
+      </nav>
+
+    { notification != "" && (
+      <div className="notification">
+        { notification }
+        <span className="close-btn" onClick={() => {setNotification("")}}>X</span>
+      </div>
+    )}
+  
+    <Routes>
+      {/*
+    <Route path="/" element={
+      <IndexComponent/>
+    }/>*/}
+    </Routes>
+  </div>
   );
 }
 
